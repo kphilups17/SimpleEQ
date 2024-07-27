806ce62be8b1d680c9d57aecab179a13b60596e3
Kyle Phillips <kphilups17@gmail.com>
2024-07-27 09:40:43 -0400

    create ResponseCurveComponent

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -22,12 +22,32 @@ struct CustomRotarySlider : juce::Slider
     }
 };
 
+struct ResponseCurveComponent : juce::Component,
+    juce::AudioProcessorParameter::Listener,
+    juce::Timer
+{
+    ResponseCurveComponent(SimpleEQAudioProcessor&);
+    ~ResponseCurveComponent() override;
+
+    void parameterValueChanged(int parameterIndex, float newValue) override;
+    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {};
+    void timerCallback() override;
+
+    void paint(juce::Graphics& g) override;
+
+
+private:
+    SimpleEQAudioProcessor& audioProcessor;
+
+    juce::Atomic<bool> parameterChanged{ false };
+
+    MonoChain monoChain;
+};
+
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor,
-juce::AudioProcessorParameter::Listener, 
-juce::Timer
+class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -37,37 +57,10 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
-    void parameterValueChanged(int parameterIndex, float newValue) override;
-
-    /** Indicates that a parameter change gesture has started.
-
-        E.g. if the user is dragging a slider, this would be called with gestureIsStarting
-        being true when they first press the mouse button, and it will be called again with
-        gestureIsStarting being false when they release it.
-
-        IMPORTANT NOTE: This will be called synchronously, and many audio processors will
-        call it during their audio callback. This means that not only has your handler code
-        got to be completely thread-safe, but it's also got to be VERY fast, and avoid
-        blocking. If you need to handle this event on your message thread, use this callback
-        to trigger an AsyncUpdater or ChangeBroadcaster which you can respond to later on the
-        message thread.
-    */
-    void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {};
-
-    //==============================================================================
-    /** The user-defined callback routine that actually gets called periodically.
-
-        It's perfectly ok to call startTimer() or stopTimer() from within this
-        callback to change the subsequent intervals.
-    */
-    void timerCallback() override;
-
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor; 
-
-    juce::Atomic<bool> parameterChanged{ false };
 
     CustomRotarySlider peakFreqSlider, 
                        peakGainSlider, 
@@ -76,6 +69,8 @@ private:
                        highCutFreqSlider,
                        lowCutSlopeSlider, 
                        highCutSlopeSlider; 
+
+    ResponseCurveComponent responseCurveComponent;
 
     using APVTS = juce::AudioProcessorValueTreeState; 
     using Attachment = APVTS::SliderAttachment;
@@ -89,8 +84,6 @@ private:
                 highCutSlopeSliderAttachment;
 
     std::vector<juce::Component*> getComps();
-
-    MonoChain monoChain;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
 };
